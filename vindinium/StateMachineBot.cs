d6f41e04119576e4ab1c2ca3f0194e9bb908cb4f
Taylor Plimpton <taylor.plimpton@smallplanet.com>
2017-06-13 18:41:18 -0400

    Smarter Bot and Better State Handling
    - Better decision making for attacking (including getting health first) - may be computationally heavy
    - Better Tavern find based on path
    - Better state handling

--- vindinium/StateMachineBot.cs
+++ vindinium/StateMachineBot.cs
@@ -150,82 +150,98 @@ namespace vindinium
 			// check nearby tiles
 
 
+			// make sure we're not currently looking to heal
+			if (currentState != MyHeroState.HEAL) {
+				/* Decision Tree:
+				 *    1) Find the closest of 3 mines based on the path
+				 *    2) Look for a nearby hero of interest (easy to kill, higher rank, )
+				 *      a) if found check the path in relation to the current mine path
+				 *        - if the path is less then mine path plus 5, attack the hero
+				 *    3) Check if my hero's heal is less then 20 after taking the current path into account OR if we're in 1st near the end of the game
+				 *      a) if the heal is less then 20 OR we're in 1st near the end of the game, we'll head for a tavern
+				 */
 
 
+				Pos heroOfInterestPos = null;
+				List<string> bestPathToHeroOfInterest = bestPathForClosestHeroOfInterestPos (out heroOfInterestPos);
+				// check if we were attacking and the current hero of interest is relatively close to our target position
+				bool attacking = heroOfInterestPos != null && bestPathToHeroOfInterest.Count > 0;
+				if (attacking && currentTargetPos != null && pathList.Count > 0) {
+					attacking = bestPathToHeroOfInterest.Count <= pathList.Count + 5;
+				}
+					
+				if (!attacking) {
+					// make sure we're not already capturing a mine
+					if (currentState != MyHeroState.CAPTURE_MINE) {
+						//Console.Out.WriteLine ("findClosestMinePos");
+						List<Pos> closestMinePosList = findClosestMinePositions (3);
+						if (closestMinePosList.Count > 0) {
+							//Console.Out.WriteLine ("findClosestMinePos count greater then 0");
 
-			if (currentState != MyHeroState.CAPTURE_MINE) {
-				//Console.Out.WriteLine ("findClosestMinePos");
-				List <Pos> closestMinePosList = findClosestMinePositions (3);
-				if (closestMinePosList.Count > 0) {
-					//Console.Out.WriteLine ("findClosestMinePos count greater then 0");
+							// find the best path for each mine so we can compare which one is truly closest
+							int smallestPathLength = 99999;
+							Pos closestMinePos = null;
+							List<string> bestPathList = null;
+							foreach (Pos currMinePos in closestMinePosList) {
+								List<string> currBestPath = bestPathToPos (currMinePos);
+								if (currBestPath.Count < smallestPathLength) {
+									smallestPathLength = currBestPath.Count;
+									bestPathList = currBestPath;
+									closestMinePos = currMinePos;
+								}
+							}
 
-					// find the best path for each mine so we can compare which one is truly closest
-					int smallestPathLength = 99999;
-					Pos closestMinePos = null;
-					List<string> bestPathList = null;
-					foreach (Pos currMinePos in closestMinePosList) {
-						List<string> currBestPath = bestPathToPos (currMinePos);
-						if (currBestPath.Count < smallestPathLength) {
-							smallestPathLength = currBestPath.Count;
-							bestPathList = currBestPath;
-							closestMinePos = currMinePos;
+							if (closestMinePos == null) {
+								Console.Out.WriteLine ("findClosestMinePositions was null");
+							} else if (currentTargetPos == null || !currentTargetPos.EqualsPos (closestMinePos)) {
+								currentState = MyHeroState.CAPTURE_MINE;
+
+								currentTargetPos = closestMinePos;
+								Console.Out.WriteLine ("currentTargetPos: " + currentTargetPos.x + ", " + currentTargetPos.y);
+								// wipe our current path and re-determine it
+								pathList.Clear ();
+
+								pathList = bestPathList;
+							}
 						}
 					}
+				} else {
+					currentState = MyHeroState.ATTACK;
+					if (currentTargetPos == null || !currentTargetPos.EqualsPos (heroOfInterestPos)) {
+						currentTargetPos = heroOfInterestPos;
+						Console.Out.WriteLine ("currentTargetPos: " + currentTargetPos.x + ", " + currentTargetPos.y);
+						// wipe our current path and re-determine it
+						pathList.Clear ();
 
-					if (closestMinePos == null) {
+						pathList = bestPathToHeroOfInterest;
+					}
+				}
+
+
+
+				// 1) Check my hero's health, if the health minus the moves to the target is less than 20 we'll need to heal first
+				// 2) Check if my hero is in first near the end of the game, if so we'll "suck our thumb"
+				bool rank1NearGameEnd = serverStuff.board.Rank == 1 && myHero.gold > 0 && (float)serverStuff.currentTurn / (float)serverStuff.maxTurns >= 0.95f;
+				if (myHero.life - pathList.Count <= 20 || rank1NearGameEnd) {
+					Console.Out.WriteLine ("myHero.life: " + myHero.life);
+					Console.Out.WriteLine ("rank1NearGameEnd: " + rank1NearGameEnd);
+
+					List<string> bestPathList = new List<string> ();
+					Pos closestTavernPos = null;
+					bestPathList = bestPathToClosestTavern (out closestTavernPos);
+
+					if (closestTavernPos == null) {
 						Console.Out.WriteLine ("findClosestMinePositions was null");
-					} else if (currentTargetPos == null || !currentTargetPos.EqualsPos (closestMinePos)) {
-						currentState = MyHeroState.CAPTURE_MINE;
+					} else if (currentTargetPos == null || !currentTargetPos.EqualsPos (closestTavernPos)) {
+						currentState = MyHeroState.HEAL;
 
-						currentTargetPos = closestMinePos;
+						currentTargetPos = closestTavernPos;
 						Console.Out.WriteLine ("currentTargetPos: " + currentTargetPos.x + ", " + currentTargetPos.y);
 						// wipe our current path and re-determine it
 						pathList.Clear ();
 
 						pathList = bestPathList;
 					}
-				}
-			}
-
-			if (currentState != MyHeroState.HEAL) {
-				//Console.Out.WriteLine ("findClosestBeatableHeroPos");
-				Pos beatableHeroPos = findClosestHeroOfInterestPos ();
-				if (beatableHeroPos == null) {
-					Console.Out.WriteLine ("findClosestBeatableHeroPos was null");
-				} else {
-					//Console.Out.WriteLine ("findClosestBeatableHeroPos not null");
-					if (currentTargetPos == null || !currentTargetPos.EqualsPos (beatableHeroPos)) {
-						currentState = MyHeroState.ATTACK;
-
-						currentTargetPos = beatableHeroPos;
-						Console.Out.WriteLine ("currentTargetPos: " + currentTargetPos.x + ", " + currentTargetPos.y);
-						// wipe our current path and re-determine it
-						pathList.Clear ();
-
-						pathList = bestPathToPos (currentTargetPos);
-					}
-				}
-			}
-
-			// 1) Make sure we're not already headed for a tavern
-			// 2) Check my hero's health, if the health minus the moves to the target is less than 20 we'll need to heal first
-			// 3) Check if my hero is in first near the end of the game, if so we'll "suck our thumb"
-			bool rank1NearGameEnd = serverStuff.board.Rank == 1 && myHero.gold > 0 && (float)serverStuff.currentTurn/(float)serverStuff.maxTurns >= 0.95f;
-			if (myHero.life - pathList.Count <= 20 || rank1NearGameEnd) {
-				Console.Out.WriteLine ("myHero.life: " + myHero.life);
-				Console.Out.WriteLine ("rank1NearGameEnd: " + rank1NearGameEnd);
-				// find a tavern
-				Pos tavernPos = findClosestTavernPos ();
-				if (tavernPos == null) {
-					Console.Out.WriteLine ("findClosestTavernPos was null");
-				} else if (currentTargetPos == null || !currentTargetPos.EqualsPos (tavernPos)) {
-					currentState = MyHeroState.HEAL;
-					currentTargetPos = tavernPos;
-					Console.Out.WriteLine ("currentTargetPos: " + currentTargetPos.x + ", " + currentTargetPos.y);
-					// wipe our current path and re-determine it
-					pathList.Clear ();
-
-					pathList = bestPathToPos (currentTargetPos);
 				}
 			}
 
@@ -503,35 +519,112 @@ namespace vindinium
 
 		#region Helper Methods
 
-		// finds the closest hero in relation to the bot (myHero)
-		// -x value is to the left or west
-		// -y value is above or to the north
-		// IMPORTANT: null Pos means there were no beatable heroes found (in other words they all have greater health then myHero)
-		private Pos findClosestHeroOfInterestPos () {
+		// finds the closest hero of interest in relation to the bot (myHero)
+		// out heroOfInterestPos is null if no hero of interest is found (or if the path finding failed, which would be a bug)
+		// hero intest based on weight (see weigh explanation in method)
+		// IMPORTANT: empty path list means there were no hero of interest found (or if the path finding failed, which would be a bug)
+		private List<string> bestPathForClosestHeroOfInterestPos (out Pos heroOfInterestPos) {
+			heroOfInterestPos = null;
+			List<string> returnPath = new List<string> ();
+
 			Hero myHero = serverStuff.myHero;
-			Pos myHeroPos = correctedHeroPos ();
-			Pos closestBeatableHeroPos = null;
+
+			// weigh our heros based on:
+			// their health vs ours
+			// their gold vs ours in relation to the turns left
+			// their mines vs ours in relation to the turns left
+			double bestHeroOfInterestWeight = 0;
+			Hero heroOfInterest = null;
 			foreach (Hero currHero in serverStuff.heroes) {
-				if (currHero.id != myHero.id) {
-					bool otherHeroHasTooMuchMoreGold = currHero.gold - myHero.gold >= 250;
-					bool otherHeroHasLessHealth = myHero.life > currHero.life;
-					/* TODO:
-					 * check if other hero has a decent amount more gold diff:
-					 * -- check if they have more health and heal up before attacking
-					 * */
-					if (closestBeatableHeroPos == null || myHeroPos.Distance (currHero.GetCorrectedHeroPos ()) < myHeroPos.Distance (closestBeatableHeroPos)) {
-						closestBeatableHeroPos = currHero.GetCorrectedHeroPos ();
-					}
+				// skip over myhero
+				if (currHero.id == myHero.id) {
+					continue;
+				}
+
+				double gameDonePercent = (double)serverStuff.currentTurn / (double)serverStuff.maxTurns;
+				double currHeroWeight = 0;
+				currHeroWeight += ((double)(currHero.gold - myHero.gold) / 100) * gameDonePercent;
+				currHeroWeight += (double)(currHero.mineCount - myHero.mineCount) * gameDonePercent;
+				currHeroWeight += myHero.pos.Distance (currHero.pos) / ((double)serverStuff.board.Length / 2); // minimal weight since obstacles can drastically effect the path distance
+				// having less health then the currHero should weigh more heavily then having more health
+				int healthDiff = myHero.life - currHero.life;
+				if (healthDiff < 1) {
+					currHeroWeight -= 5;
+				} else if (healthDiff < 10) {
+					currHeroWeight -= 1;
+				}
+
+				if (bestHeroOfInterestWeight < currHeroWeight) {
+					bestHeroOfInterestWeight = currHeroWeight;
+					heroOfInterest = currHero;
+				}
+			}
+				
+			if (heroOfInterest != null) {
+				Pos startPos = myHero.GetCorrectedHeroPos ();
+				heroOfInterestPos = heroOfInterest.GetCorrectedHeroPos ();
+
+				// do we have enough health to beat this hero
+				if (myHero.life - heroOfInterest.life < 1) {
+					// we'll need to find a tavern first
+					Pos closestTavernPos = null;
+					returnPath = bestPathToClosestTavern (out closestTavernPos);
+
+					if (closestTavernPos != null)
+						startPos = closestTavernPos;
+				}
+
+				List<string> bestPathToHeroOfInterest = new List<string> ();
+				if (findBestPathFromPosToPos (startPos, heroOfInterestPos, out bestPathToHeroOfInterest))
+					returnPath.AddRange (bestPathToHeroOfInterest);
+				else {
+					heroOfInterestPos = null;
+					returnPath.Clear ();
 				}
 			}
 
-			return closestBeatableHeroPos;
+			return returnPath;
+
+
+
+
+//			Pos myHeroPos = correctedHeroPos ();
+//			Pos closestBeatableHeroPos = null;
+//			int greatestDiffInGold = 0;
+//			double currBeatableHeroDist = 99999;
+//			foreach (Hero currHero in serverStuff.heroes) {
+//				if (currHero.id != myHero.id) {
+//					// dist
+//					double currHeroDist = myHeroPos.Distance (currHero.GetCorrectedHeroPos ());
+//					// does this hero have more gold than me by at least 250
+//					bool otherHeroHasTooMuchMoreGold = currHero.gold - myHero.gold >= 250;
+//					// does this hero have less health then me
+//					bool otherHeroHasLessHealth = myHero.life > currHero.life;
+//
+//					/* TODO:
+//					 * check if other hero has a decent amount more gold diff:
+//					 * -- check if they have more health and heal up before attacking
+//					 * */
+//
+//					// best case scenario
+//					if (otherHeroHasLessHealth && otherHeroHasTooMuchMoreGold) {
+//						List<string> currPath
+//					}
+//					if (otherHeroHasTooMuchMoreGold) {
+//
+//					if (!otherHeroHasLessHealth) {
+//						// other hero does
+//					}
+//				}
+//			}
+//
+//			return closestBeatableHeroPos;
 		}
 
-		// finds the closest mine in relation to the bot (myHero)
-		// -x value is to the left or west
-		// -y value is above or to the north
-		// IMPORTANT: null Pos means there were no neutral mines found (in other words they're all controlled)
+		// finds the closest mine(s) in relation to the bot (myHero)
+		// argument amountToFind: amount of mines to compare/find
+		// retuns a list of board position(s) of the sorted mine(s) based on distance
+		// IMPORTANT: null Pos means there were no mines found (in other words my hero controls them all)
 		private List<Pos> findClosestMinePositions (int amountToFind = 1) {
 			List <Pos> returnList = new List<Pos> ();
 
@@ -581,20 +674,53 @@ namespace vindinium
 			return returnList;
 		}
 
-		// finds the closest tavern in relation to the bot (myHero)
-		// -x value is to the left or west
-		// -y value is above or to the north
-		// IMPORTANT: should never be the case but null Pos means there were no taverns found (in other words this map doesn't have any taverns)
-		private Pos findClosestTavernPos () {
-			Pos myHeroPos = correctedHeroPos ();
-			Pos closestTavernPos = null;
-			foreach (Pos currTavernPos in tavernsPosList) {
-				if (closestTavernPos == null || myHeroPos.Distance (currTavernPos) < myHeroPos.Distance (closestTavernPos)) {
-					closestTavernPos = currTavernPos;
+		// finds the best/shortest path to a tavern
+		// out closestTavernPos is null if no tavern is found (or if the path finding failed, which would be a bug)
+		// retuns the path list of string moves
+		// IMPORTANT: empty path list means there was no tavern found (or if the path finding failed, which would be a bug)
+		private List<string> bestPathToClosestTavern (out Pos closestTavernPos) {
+			closestTavernPos = null;
+			List<string> bestPathList = new List<string> ();
+
+			// find a tavern
+			List<Pos> closestTavernPosList = findClosestTavernPositions (2);
+			if (closestTavernPosList.Count > 0) {
+				//Console.Out.WriteLine ("findClosestTavernPositions count greater then 0");
+
+				// find the best path for each tavern so we can compare which one is truly closest
+				int smallestPathLength = 99999;
+				foreach (Pos currTavernPos in closestTavernPosList) {
+					List<string> currBestPath = bestPathToPos (currTavernPos);
+					if (currBestPath.Count < smallestPathLength) {
+						smallestPathLength = currBestPath.Count;
+						bestPathList = currBestPath;
+						closestTavernPos = currTavernPos;
+					}
 				}
+
 			}
 
-			return closestTavernPos;
+			return bestPathList;
+		}
+
+		// finds the closest tavern(s) in relation to the bot (myHero)
+		// argument amountToFind: amount of taverns to compare/find
+		// retuns a list of board position(s) of the sorted tavern(s) based on distance
+		// IMPORTANT: should never be the case but null Pos means there were no taverns found (in other words this map doesn't have any taverns)
+		private List<Pos> findClosestTavernPositions (int amountToFind = 1) {
+			Pos myHeroPos = correctedHeroPos ();
+
+			// sort the list of the mines in relation to the current hero's pos
+			List <Pos> sortedList = new List<Pos> (tavernsPosList);
+			sortedList.Sort (delegate (Pos a, Pos b) {
+				return myHeroPos.Distance(a).CompareTo (myHeroPos.Distance(b));
+			});
+
+			if (amountToFind > sortedList.Count) {
+				return sortedList;
+			}
+
+			return sortedList.GetRange (0, amountToFind);
 		}
 
 		// determines the offset from the bot (myHero) to another tile position
